3a46e1f2de464cb772ad0dacca8c5ba55301194c
rande <randelvondexus@gmail.com>
2019-05-21 10:37:16 +0545

    initial commit

--- app/src/main/java/com/example/heroapi/MainActivity.java
+++ app/src/main/java/com/example/heroapi/MainActivity.java
@@ -5,6 +5,7 @@ import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
+import android.os.StrictMode;
 import android.provider.MediaStore;
 import android.support.annotation.Nullable;
 import android.support.v4.content.CursorLoader;
@@ -18,14 +19,22 @@ import android.widget.ImageView;
 import android.widget.Toast;
 
 import java.io.File;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 import heroesapi.HeroesAPI;
 import model.Heroes;
+import model.ImageResponse;
+import okhttp3.MediaType;
+import okhttp3.MultipartBody;
+import okhttp3.RequestBody;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
+import retrofit2.http.Multipart;
 import url.Url;
 
 public class MainActivity extends AppCompatActivity {
@@ -33,6 +42,7 @@ public class MainActivity extends AppCompatActivity {
     private Button btnsave;
     private ImageView imgProfile;
     String imagePath;
+    String imageName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -76,6 +86,7 @@ public class MainActivity extends AppCompatActivity {
         Uri uri=data.getData();
         imagePath=getRealPathFromUri(uri);
         previewImage(imagePath);
+
     }
 
 
@@ -99,8 +110,14 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void Save() {
+        SaveImageOnly();
         String name = etname.getText().toString();
         String desc = etdesc.getText().toString();
+
+        Map<String, String> map = new HashMap<>();
+        map.put("name", name);
+        map.put("desc", desc);
+        map.put("image", imageName);
 
         Heroes heroes = new Heroes(name, desc);
 
@@ -109,6 +126,7 @@ public class MainActivity extends AppCompatActivity {
 
         HeroesAPI heroesAPI = retrofit.create(HeroesAPI.class);
         Call<Void> heroesCall = heroesAPI.addHero(heroes);
+
         heroesCall.enqueue(new Callback<Void>() {
             @Override
             public void onResponse(Call<Void> call, Response<Void> response) {
@@ -128,5 +146,29 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+
+    private void StrictMode() {
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
+    }
+
+    private void SaveImageOnly(){
+
+        File file = new File(imagePath);
+        RequestBody requestBody = RequestBody.create(MediaType.parse("multipart/form-data"), file);
+        MultipartBody.Part body = MultipartBody.Part.createFormData("imageFile", file.getName(),requestBody);
+
+        HeroesAPI heroesAPI = Url.getInstance().create(HeroesAPI.class);
+        Call<ImageResponse> responseBodyCall = heroesAPI.uploadImage(body);
+        StrictMode();
+
+        try {
+            Response<ImageResponse> imageResponseResponse = responseBodyCall.execute();
+            imageName = imageResponseResponse.body().getFilename();
+        } catch (IOException e) {
+            Toast.makeText(this, "Error", Toast.LENGTH_SHORT).show();
+            e.printStackTrace();
+        }
+    }
 }
 
--- app/src/main/java/heroesapi/HeroesAPI.java
+++ app/src/main/java/heroesapi/HeroesAPI.java
@@ -1,14 +1,20 @@
 package heroesapi;
 
+import java.util.List;
 import java.util.Map;
 
 import model.Heroes;
+import model.ImageResponse;
+import okhttp3.MultipartBody;
 import retrofit2.Call;
 import retrofit2.http.Body;
 import retrofit2.http.Field;
 import retrofit2.http.FieldMap;
 import retrofit2.http.FormUrlEncoded;
+import retrofit2.http.GET;
+import retrofit2.http.Multipart;
 import retrofit2.http.POST;
+import retrofit2.http.Part;
 
 public interface HeroesAPI {
 
@@ -23,5 +29,12 @@ public interface HeroesAPI {
     @FormUrlEncoded
     @POST("heroes")
     Call<Void> addHero (@FieldMap Map<String,String> map);
+
+    @Multipart
+    @POST("upload")
+    Call<ImageResponse> uploadImage(@Part MultipartBody.Part img );
+
+    @GET("heroes")
+    Call<List<Heroes>> getAllHeroes();
 
 }
